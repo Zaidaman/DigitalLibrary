2d0714f267a38c483bc1abbcf2cfe7b8a3c6f535
Luca Vignali <lucavigno2003@gmail.com>
2025-10-31 17:06:35 +0100

    Update Book.java

--- src/main/java/com/library/models/Book.java
+++ src/main/java/com/library/models/Book.java
@@ -3,14 +3,27 @@ package com.library.models;
 import java.io.File;
 
 public class Book {
+    private int id;
     private String title;
-    private File file;
+    private String author;
+    private String genre;
+    private String subgenre;
+    private String description;
+    private File data;
 
-    public Book(String title, File file) {
+    public Book(String title, String author, String genre, String subgenre, String description, File data) {
         this.title = title;
-        this.file = file;
+        this.author = author;
+        this.genre = genre;
+        this.subgenre = subgenre;
+        this.description = description;
+        this.data = data;
     }
 
     public String getTitle() { return title; }
-    public File getFile() { return file; }
+    public String getAuthor() { return author; }
+    public String getGenre() { return genre; }
+    public String getSubgenre() { return subgenre; }
+    public String getDescription() { return description; }
+    public File getData() { return data; }
 }
