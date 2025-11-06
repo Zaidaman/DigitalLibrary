590eddef0d17f74bcb2cb7353dd1c5fb8d1351ec
Luca Vignali <lucavigno2003@gmail.com>
2025-11-06 16:36:10 +0100

    aligned var name with DB

--- src/main/java/com/library/models/Book.java
+++ src/main/java/com/library/models/Book.java
@@ -1,27 +1,30 @@
 package com.library.models;
 
 public class Book {
-    private int id;
-    private String title;
-    private String author;
-    private String genre;
-    private String subgenre;
-    private String description;
-    private String fileDir;
+    private int IdBook;
+    private String Title;
+    private String Author;
+    private String Genre;
+    private String Subgenre;
+    private String Description;
+    private String FileDir;
+    private int IdLibrary;
 
-    public Book(String title, String author, String genre, String subgenre, String description, String fileDir) {
-        this.title = title;
-        this.author = author;
-        this.genre = genre;
-        this.subgenre = subgenre;
-        this.description = description;
-        this.fileDir = fileDir;
+    public Book(String title, String author, String genre, String subgenre, String description, String fileDir, int idLibrary) {
+        this.Title = title;
+        this.Author = author;
+        this.Genre = genre;
+        this.Subgenre = subgenre;
+        this.Description = description;
+        this.FileDir = fileDir;
+        this.IdLibrary = idLibrary;
     }
 
-    public String getTitle() { return title; }
-    public String getAuthor() { return author; }
-    public String getGenre() { return genre; }
-    public String getSubgenre() { return subgenre; }
-    public String getDescription() { return description; }
-    public String getFileDir() { return fileDir; }
+    public String getTitle() { return Title; }
+    public String getAuthor() { return Author; }
+    public String getGenre() { return Genre; }
+    public String getSubgenre() { return Subgenre; }
+    public String getDescription() { return Description; }
+    public String getFileDir() { return FileDir; }
+    public int getIdLibrary() { return IdLibrary; }
 }
