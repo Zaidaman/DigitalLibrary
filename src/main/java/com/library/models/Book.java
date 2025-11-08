86d4db8aef69811317cc4c0d3a47caa903b56203
Luca Vignali <lucavigno2003@gmail.com>
2025-11-08 14:59:49 +0100

    updated Db

--- src/main/java/com/library/models/Book.java
+++ src/main/java/com/library/models/Book.java
@@ -5,26 +5,21 @@ public class Book {
     private String Title;
     private String Author;
     private String Genre;
-    private String Subgenre;
-    private String Description;
-    private String FileDir;
+    private byte[] File;
     private int IdLibrary;
 
-    public Book(String title, String author, String genre, String subgenre, String description, String fileDir, int idLibrary) {
+
+    public Book(String title, String author, String genre, byte[] File, int idLibrary) {
         this.Title = title;
         this.Author = author;
         this.Genre = genre;
-        this.Subgenre = subgenre;
-        this.Description = description;
-        this.FileDir = fileDir;
+        this.File = File;
         this.IdLibrary = idLibrary;
     }
 
     public String getTitle() { return Title; }
     public String getAuthor() { return Author; }
     public String getGenre() { return Genre; }
-    public String getSubgenre() { return Subgenre; }
-    public String getDescription() { return Description; }
-    public String getFileDir() { return FileDir; }
+    public byte[] getFile() { return File; }
     public int getIdLibrary() { return IdLibrary; }
 }
--- src/mysql_query/MySQL_create.sql
+++ src/mysql_query/MySQL_create.sql
@@ -1,19 +1,46 @@
 create database if not exists DigitalLibrary;
 use DigitalLibrary;
 
+create table if not exists LibUser (
+IdUser int auto_increment primary key,
+Username varchar(20) not null unique,
+UserPass varchar(15) not null
+);
+
 create table if not exists `Library` (
 IdLibrary int auto_increment primary key,
-Name varchar(20) not null unique
+LibName varchar(20) not null unique,
+IdUser int not null default 1,
+foreign key(IdUser) references LibUser(IdUser)
+);
+
+create table if not exists Genre(
+IdGenre int auto_increment primary key,
+GenreName varchar(20) not null unique
+);
+
+create table if not exists Author(
+IdAuthor int auto_increment primary key,
+AuthorName varchar(20) not null,
+MidName varchar(20),
+Surname varchar(20) not null
 );
 
 create table if not exists Book (
 IdBook int auto_increment primary key,
 Title varchar(50) not null,
-Author varchar(30) not null,
-Genre varchar(15) not null,
-Subgenre varchar(15),
-Description varchar(100),
-FileDir varchar(100) not null,
+IdAuthor int not null default 1,
+AnnoPub int,
+BookFile mediumblob not null,
 IdLibrary int not null default 1,
+foreign key(IdAuthor) references Author(IdAuthor),
 foreign key(IdLibrary) references `Library`(IdLibrary)
+);
+
+create table if not exists BookGenre (
+IdBook int not null,
+IdGenre int not null,
+primary key(IdBook, IdGenre),
+foreign key(IdBook) references Book(IdBook),
+foreign key(IdGenre) references Genre(IdGenre)
 );
